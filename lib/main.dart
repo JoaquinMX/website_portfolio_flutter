60faeb7bd2753ae632889fc49f5f147b5c1f56b4
Joaquin Beltran <contact@joaquinmx.com>
2023-10-28 20:32:11 -0600

    feat: Added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,28 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_portfolio/firebase_options.dart';
 import 'package:flutter_portfolio/routes.dart';
 import 'package:url_strategy/url_strategy.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: FirebaseOptions(
+            apiKey: dotenv.env['API_KEY_FIREBASE'] ?? "",
+            authDomain: dotenv.env['AUTH_DOMAIN_FIREBASE'] ?? "",
+            projectId: dotenv.env['PROJECT_ID_FIREBASE'] ?? "",
+            storageBucket: dotenv.env['STORAGE_BUCKET_FIREBASE'] ?? "",
+            messagingSenderId: dotenv.env['MESSAGING_SENDER_ID_FIREBASE'] ?? "",
+            appId: dotenv.env['APP_ID_FIREBASE'] ?? "",
+            measurementId: dotenv.env['MEASUREMENT_ID_FIREBASE'] ?? ""));
+  } else {
+    await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform);
+  }
   setPathUrlStrategy();
   runApp(const MyApp());
 }
--- lib/mobile/landing_page_mobile.dart
+++ lib/mobile/landing_page_mobile.dart
@@ -20,133 +20,135 @@ class _LandingPageMobileState extends State<LandingPageMobile> {
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
 
-    return Scaffold(
-      extendBodyBehindAppBar: true,
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        iconTheme: const IconThemeData(
-          size: 35,
-          color: Colors.black,
+    return SafeArea(
+      child: Scaffold(
+        extendBodyBehindAppBar: true,
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          iconTheme: const IconThemeData(
+            size: 35,
+            color: Colors.black,
+          ),
         ),
-      ),
-      endDrawer: const CustomDrawerMobile(),
-      body: ListView(
-        children: [
-          const CircleAvatar(
-            radius: 117,
-            backgroundColor: Colors.tealAccent,
-            child: CircleAvatar(
-              radius: 110,
-              backgroundColor: Colors.white,
-              backgroundImage:
-                  AssetImage("assets/imgs/profile_picture_circle.png"),
+        endDrawer: const CustomDrawerMobile(),
+        body: ListView(
+          children: [
+            const CircleAvatar(
+              radius: 117,
+              backgroundColor: Colors.tealAccent,
+              child: CircleAvatar(
+                radius: 110,
+                backgroundColor: Colors.white,
+                backgroundImage:
+                    AssetImage("assets/imgs/profile_picture_circle.png"),
+              ),
             ),
-          ),
-          // First Section
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const SizedBox(
-                  height: 25,
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      decoration: const BoxDecoration(
-                        color: Colors.tealAccent,
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(
-                            20,
-                          ),
-                          topRight: Radius.circular(
-                            20,
-                          ),
-                          bottomRight: Radius.circular(
-                            20,
+            // First Section
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(
+                    height: 25,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        decoration: const BoxDecoration(
+                          color: Colors.tealAccent,
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(
+                              20,
+                            ),
+                            topRight: Radius.circular(
+                              20,
+                            ),
+                            bottomRight: Radius.circular(
+                              20,
+                            ),
                           ),
                         ),
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 10, horizontal: 20),
+                        child: Sans(text: "Hello I'm", size: 15, isBold: true),
                       ),
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 10, horizontal: 20),
-                      child: Sans(text: "Hello I'm", size: 15, isBold: true),
-                    ),
-                    Sans(text: "Joaquin Beltran", size: 40, isBold: true),
-                    Sans(text: "Flutter Developer", size: 20),
-                  ],
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    const Wrap(
-                      direction: Axis.vertical,
-                      spacing: 3,
-                      children: [
-                        Icon(
-                          Icons.email,
-                        ),
-                        Icon(
-                          Icons.call,
-                        ),
-                        Icon(
-                          Icons.location_pin,
-                        ),
-                      ],
-                    ),
-                    const SizedBox(
-                      width: 40,
-                    ),
-                    Wrap(
-                      direction: Axis.vertical,
-                      spacing: 9,
-                      children: [
-                        Sans(
-                          text: "contact@joaquinmx.com",
-                          size: 15,
-                        ),
-                        Sans(text: "(123) 456-7890", size: 15),
-                        Sans(
-                          text: "North America",
-                          size: 15,
-                        )
-                      ],
-                    )
-                  ],
-                )
-              ],
+                      Sans(text: "Joaquin Beltran", size: 40, isBold: true),
+                      Sans(text: "Flutter Developer", size: 20),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      const Wrap(
+                        direction: Axis.vertical,
+                        spacing: 3,
+                        children: [
+                          Icon(
+                            Icons.email,
+                          ),
+                          Icon(
+                            Icons.call,
+                          ),
+                          Icon(
+                            Icons.location_pin,
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        width: 40,
+                      ),
+                      Wrap(
+                        direction: Axis.vertical,
+                        spacing: 9,
+                        children: [
+                          Sans(
+                            text: "contact@joaquinmx.com",
+                            size: 15,
+                          ),
+                          Sans(text: "(123) 456-7890", size: 15),
+                          Sans(
+                            text: "North America",
+                            size: 15,
+                          )
+                        ],
+                      )
+                    ],
+                  )
+                ],
+              ),
             ),
-          ),
-          const SizedBox(
-            height: 90,
-          ),
-          // Second Section
-          const AboutMeSectionMobile(),
-          const SizedBox(
-            height: 60,
-          ),
-          // Third Section
-          ServicesSectionMobile(
-            width: width,
-            service1: service1(width),
-            service2: service2(width),
-            service3: service3(width),
-          ),
-          const SizedBox(
-            height: 60,
-          ),
-          // Fourth Section
-          ContactMeMobile(width: width),
-          const SizedBox(
-            height: 20,
-          )
-        ],
+            const SizedBox(
+              height: 90,
+            ),
+            // Second Section
+            const AboutMeSectionMobile(),
+            const SizedBox(
+              height: 60,
+            ),
+            // Third Section
+            ServicesSectionMobile(
+              width: width,
+              service1: service1(width),
+              service2: service2(width),
+              service3: service3(width),
+            ),
+            const SizedBox(
+              height: 60,
+            ),
+            // Fourth Section
+            ContactMeMobile(width: width),
+            const SizedBox(
+              height: 20,
+            )
+          ],
+        ),
       ),
     );
   }
